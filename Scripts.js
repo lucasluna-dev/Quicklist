dfc63612c59bf34660ebb6fc5f90338eb40d508f
luna <lucassilvavg9@gmail.com>
2026-01-01 22:10:30 -0300

    inputs coletados para variavel

--- Scripts.js
+++ Scripts.js
@@ -1,1 +1,26 @@
-console.log("vasco")
+
+const item = document.getElementById("item")
+const number = document.getElementById("quantity")
+const categoria = document.getElementById("Categoria")
+const form = document.querySelector("form")
+
+const listDeCompras = []
+const dataAtual = new Date();
+
+
+
+form.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    
+    const novoItem = 
+    {
+        "id": Date.now(),
+        "nomeItem": item.value,
+        "quantityItem":number.value,
+        "categoriaItem":categoria.value
+    };
+
+    console.log(novoItem.id)
+    
+}) 
+
